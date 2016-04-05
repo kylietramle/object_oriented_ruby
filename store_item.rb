411581311355e91d8d933a79108e49b75d21b08e
Kylie Le <kylie.tram.le@gmail.com>
2016-04-04 20:39:58 -0700

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,18 @@
+stuffed_animals = [
+  { type: "pig",
+    color: "white and pink",
+    price: 50,
+  },
+
+  { type: "cow",
+    color: "white and black",
+    price: 30,
+  },
+
+  { :type => "bunny",
+    :color => "white",
+    :price => 35,
+  }
+]
+
+p stuffed_animals
