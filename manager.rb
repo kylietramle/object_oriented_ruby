59ce1a7b74d3ec3bf8021b7a8a5057a84ecf6fa6
Kylie Le <kylie.tram.le@gmail.com>
2016-04-07 18:30:19 -0700

    Module Example initial commit

--- manager.rb
+++ manager.rb
@@ -1,12 +1,25 @@
+module EmailReporter
+   def send_report
+    puts "Sending email..."
+    # use email sending library...
+    puts "Email sent!"
+  end
+end
+
 class Employee
   attr_reader :first_name, :last_name, :salary
-  attr_accessor :active
+  #attr_accessor :active
+  attr_writer :active
 
   def initialize(input_options)
     @first_name = input_options[:first_name]
     @last_name = input_options[:last_name]
     @salary = input_options[:salary]
     @active = input_options[:active]
+  end
+
+  def active
+    @active
   end
 
   def print_info
@@ -19,17 +32,12 @@ class Employee
 end
 
 class Manager < Employee
+  include EmailReporter
   attr_reader :employees
 
   def initialize(input_options)
     super(input_options)
     @employees = input_options[:employees] # give an array, see line 56
-  end
-
-  def send_report
-    puts "Sending email..."
-    # use email sending library...
-    puts "Email sent!"
   end
 
   def give_all_raises
@@ -45,10 +53,13 @@ class Manager < Employee
     @employees.each do |employee|
       employee.active = false
 
-      #p employee.active << for this to work, :active must be attr_accessor
+      p employee.active #<< for this to work, :active must be attr_accessor
     end
-
   end
+end
+
+class Intern < Employee
+  include EmailReporter
 end
 
 employee1 = Employee.new({first_name: "Majora", last_name: "Carter", salary: 80000, active: true})
@@ -59,5 +70,5 @@ employee2.print_info
 manager = Manager.new({first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2]})
 manager.print_info
 manager.send_report
-p manager.give_all_raises
+# p manager.give_all_raises
 p manager.fire_all_employees
---
+++ module_example.rb
@@ -0,0 +1,45 @@
+module Vehicle
+  attr_reader :speed, :direction
+
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car
+  include Vehicle
+  def honk_horn
+    "Beeeeeeep!"
+  end
+end
+
+class Bike
+  include Vehicle
+  def ring_bell
+    "Ring ring!"
+  end
+end
+
+willy_car = Car.new
+willy_bike = Bike.new
+
+willy_car.accelerate
+p "Willy_car accelerates to #{willy_car.speed}, #{willy_car.direction}"
+willy_bike.accelerate
+p "Willy_bike accelerates to #{willy_bike.speed}, #{willy_bike.direction}"
+
+puts "Willy_car goes #{willy_car.honk_horn}"
+puts "Willy_bike goes #{willy_bike.ring_bell}"
